ced7dba5791702f3b87d51cb3937c6643a5bc907
athanik <nikolaus.athanassiadis@eniro.com>
2012-09-05 10:02:46 +0200

    APOA icon

--- org.apoa.core/src/org/apoa/core/QueryProcessManager.java
+++ org.apoa.core/src/org/apoa/core/QueryProcessManager.java
@@ -14,13 +14,36 @@ public class QueryProcessManager {
 	private URL url;
 	private URLGenerator urlGenerator = new DuckDuckGoUrlGenerator();
 	
+	private static final String [] SEARCH_PHRASES = {"\nRoot cause:","\nCaused by:"};
+	private static final String MAIN_EXCEPTION_PHRASE = "Exception in thread";
 	public QueryProcessManager(){
-		wtfQuery = "java tutorial";
+		
+		String consoleContents = getConsoleLog();
+		wtfQuery = parseConsole(consoleContents);
 		
 		url = urlGenerator.createUrl(toUtf8(wtfQuery));
 		
 		openInternalBrowser(url);
+	}
+
+	private String parseConsole(String consoleContents) {
+		String result = "";
+		for(String phrase : SEARCH_PHRASES) {
+			int index = consoleContents.lastIndexOf(phrase);
+			if(!(index < 0)) {
+				index++;
+				int lineEnd = consoleContents.substring(index).indexOf('\n');
+				result = consoleContents.substring(index,index+lineEnd).trim();
+				break;
+			}
+		}
 		
+		
+		int indexMain = consoleContents.lastIndexOf(MAIN_EXCEPTION_PHRASE);
+		String consoleExceptionLine = consoleContents.substring(indexMain,consoleContents.substring(indexMain).indexOf("\n")).replaceAll("\\\".*?\\\"", "");
+		
+		//extract the main exception
+		return consoleExceptionLine + " "+result;
 	}
 
 	private String toUtf8(String string) {
@@ -43,19 +66,16 @@ private void openInternalBrowser(URL url) {
 }
 
 
-	private String getConsoleLog() {
-	
-		return null;
-	}
 
-	   private void findConsole() {
+	   private String getConsoleLog() {
 		      ConsolePlugin plugin = ConsolePlugin.getDefault();
 		      IConsoleManager conMan = plugin.getConsoleManager();
 		      IConsole[] existing = conMan.getConsoles();
-		      System.out.println("----------------------------");
+//		      System.out.println("----------------------------");
 		      int latest = existing.length-1;
 		      IOConsole ic = (IOConsole)existing[latest];
-		            System.out.println("Console name :" + ic.getName() + "\nConsole type: " + ic.getType() + "\n\n***********************************" +ic.getDocument().get());
-		      //no console found, so create a new one
+//		            System.out.println("Console name :" + ic.getName() + "\nConsole type: " + ic.getType() + "\n\n***********************************" +ic.getDocument().get());
+		      return ic.getDocument().get();
+		            //no console found, so create a new one
 		   }
 }
